f8d96aff03704dd625604379bc6328fe50f99d74
Jon Purdy <evincarofautumn@gmail.com>
2012-08-06 21:29:47 -0700

    Separate positive from negative tests.

--- run-tests.sh
+++ run-tests.sh
@@ -1,33 +1,37 @@
 #!/bin/bash
 
-function run_warn {
-    set +e +E
-    pass=true
-
-    diff -u --strip-trailing-cr -- \
-        "./compiler/tests/$1.warn" "./build/tests.warn/$1"
-    if [ "$?" != 0 ] ; then pass=false; fi
-
-    if ! $pass ; then
-	echo "[FAILED] $1"
-	exit 1
-    fi
-
-    set -e -E
-}
+# function run_warn {
+#     set +e +E
+#     pass=true
+# 
+#     diff -u --strip-trailing-cr -- \
+#         "./compiler/tests/negative/$1.warn" "./build/tests.warn/$1"
+#     if [ "$?" != 0 ] ; then pass=false; fi
+# 
+#     if ! $pass ; then
+# 	echo "[FAILED] $1"
+# 	exit 1
+#     fi
+# 
+#     set -e -E
+# }
 
 function run_test {
     set +e +E
     pass=true
 
-    "./build/tests/$1" > "./build/tests.out/$1" 2> "./build/tests.err/$1"
+    "${args[1]}/$1" \
+        > "${args[1]}.out/$1" \
+        2> "${args[1]}.err/$1"
 
     diff -u --strip-trailing-cr -- \
-        "./compiler/tests/$1.out" "./build/tests.out/$1"
+        "${args[0]}/$1.out" \
+        "${args[1]}.out/$1"
     if [ "$?" != 0 ] ; then pass=false; fi
 
     diff -u --strip-trailing-cr -- \
-        "./compiler/tests/$1.err" "./build/tests.err/$1"
+        "${args[0]}/$1.err" \
+        "${args[1]}.err/$1"
     if [ "$?" != 0 ] ; then pass=false; fi
 
     if ! $pass ; then
@@ -38,17 +42,26 @@ function run_test {
     set -e -E
 }
 
-for file in ./build/tests.warn/* ; do
-    if [ -f "$file" ]; then
-        run_warn $(basename "$file")
-    else
-        echo "Skipping test $file ..."
-    fi
-done
+if [ $# != 2 ]; then
+    echo 'Usage: run-tests.sh SourceDirectory TargetDirectory'
+    exit 1
+fi
 
-for file in ./build/tests/* ; do
+args=("$@")
+
+# for file in ./build/tests.warn/* ; do
+#     if [ -f "$file" ]; then
+#         run_warn $(basename "$file")
+#     else
+#         echo "Skipping test $file ..."
+#     fi
+# done
+
+for file in ./build/tests/positive/* ; do
     if [ -f "$file" ]; then
-        run_test $(basename "$file")
+        justfile=$(basename "$file")
+        echo "Running test $justfile ..."
+        run_test "$justfile"
     else
         echo "Skipping test $file ..."
     fi
